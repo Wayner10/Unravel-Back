d546831dc7623f5fac93615bed2d90f6d0221294
Wayner10 <waynerwilson49@gmail.com>
2024-11-07 14:47:53 -0600

    7-11-24

--- routes/places.js
+++ routes/places.js
@@ -57,7 +57,7 @@ place.post("/api/v1/places", async (req, res) => {
     try
     {
         //* - - - </> [QUERY] </> - - - *//
-        const data = await database.query(`INSERT INTO tb_places (place_name, place_desc, place_score, place_email, place_phone, place_price_adult, place_price_children, place_lat, place_lng, place_canton, place_nearest_city, place_waze_url, place_google_url, place_website_url, place_open_time, place_close_time, place_type_id, region_id, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19) RETURNING *`, [req.body.place_name, req.body.place_desc, req.body.place_score, req.body.place_email, req.body.place_phone, req.body.place_price_adult, req.body.place_price_children, req.body.place_lat, req.body.place_lng, req.body.place_canton, req.body.place_nearest_city, req.body.place_waze_url, req.body.place_google_url, req.body.place_website_url, req.body.place_open_time, req.body.place_close_time, req.body.place_type_id, req.body.region_id, req.body.user_id]);
+        const data = await database.query(`INSERT INTO tb_places (place_name, place_desc_short, place_desc_large, place_score, place_email, place_phone, place_price_adult, place_price_child, place_lat, place_lng, place_waze_url, place_page_url, place_opening, place_closing, place_opening_time, place_closing_time, place_feature, place_icon, place_status, place_type_id, region_id, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22) RETURNING *`, [req.body.place_name, req.body.place_desc_short, req.body.place_desc_large, req.body.place_score, req.body.place_email, req.body.place_phone, req.body.place_price_adult, req.body.place_price_child, req.body.place_lat, req.body.place_lng, req.body.place_waze_url, req.body.place_page_url, req.body.place_opening, req.body.place_closing, req.body.place_opening_time, req.body.place_closing_time, req.body.place_feature, req.body.place_icon, req.body.place_status, req.body.place_type_id, req.body.region_id, req.body.user_id]);
         console.log(data);
 
         //* - - - </> [DATA] </> - - - *//
@@ -72,13 +72,13 @@ place.post("/api/v1/places", async (req, res) => {
 
 });
 
-//* - - - </> [POST] </> - - - *//
+//* - - - </> [PUT] </> - - - *//
 place.put("/api/v1/places/:id", async (req, res) => {
 
     try
     {
         //* - - - </> [QUERY] </> - - - *//
-        const data = await database.query(`UPDATE tb_places SET place_name = $1, place_desc = $2, place_score = $3, place_email = $4, place_phone = $5, place_price_adult = $6, place_price_children = $7, place_lat = $8, place_lng = $9, place_canton = $10, place_nearest_city = $11, place_waze_url = $12, place_google_url = $13, place_website_url = $14, place_open_time = $15, place_close_time = $16, place_type_id = $17, region_id = $18, user_id = $19 WHERE place_id = $20 RETURNING *`, [req.body.place_name, req.body.place_desc, req.body.place_score, req.body.place_email, req.body.place_phone, req.body.place_price_adult, req.body.place_price_children, req.body.place_lat, req.body.place_lng, req.body.place_canton, req.body.place_nearest_city, req.body.place_waze_url, req.body.place_google_url, req.body.place_website_url, req.body.place_open_time, req.body.place_close_time, req.body.place_type_id, req.body.region_id, req.body.user_id, req.params.id]);
+        const data = await database.query(`UPDATE tb_places SET place_name = $1, place_desc_short = $2, place_desc_large = $3, place_score = $4, place_email = $5, place_phone = $6, place_price_adult = $7, place_price_child = $8, place_lat = $9, place_lng = $10, place_waze_url = $11, place_page_url = $12, place_opening = $13, place_closing = $14, place_opening_time = $15, place_closing_time = $16, place_feature = $17, place_icon = $18, place_status = $19, place_type_id = $20, region_id = $21, user_id = $22 WHERE place_id = $23 RETURNING *`, [req.body.place_name, req.body.place_desc_short, req.body.place_desc_large, req.body.place_score, req.body.place_email, req.body.place_phone, req.body.place_price_adult, req.body.place_price_child, req.body.place_lat, req.body.place_lng, req.body.place_waze_url, req.body.place_page_url, req.body.place_opening, req.body.place_closing, req.body.place_opening_time, req.body.place_closing_time, req.body.place_feature, req.body.place_icon, req.body.place_status, req.body.place_type_id, req.body.region_id, req.body.user_id, req.params.id]);
         console.log(data);
 
         //* - - - </> [DATA] </> - - - *//
--- routes/regions.js
+++ routes/regions.js
@@ -57,8 +57,7 @@ region.post("/api/v1/regions", async (req, res) => {
     try
     {
         //* - - - </> [QUERY] </> - - - *//
-        const data = await database.query(`INSERT INTO tb_regions (region_name, region_desc, region_lat, region_lng, region_featured_city) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [req.body.region_name, req.body.region_desc, req.body.region_lat, req.body.region_lng, req.body.region_featured_city]);
-        console.log(data);
+        const data = await database.query(`INSERT INTO tb_regions (region_name, region_desc, region_lat, region_lng) VALUES ($1, $2, $3, $4) RETURNING *`, [req.body.region_name, req.body.region_desc, req.body.region_lat, req.body.region_lng]);
 
         //* - - - </> [DATA] </> - - - *//
         res.status(200).json({ status: "Region successfully created", data: { region: data.rows[0]} });
@@ -78,8 +77,7 @@ region.put("/api/v1/regions/:id", async (req, res) => {
     try
     {
         //* - - - </> [QUERY] </> - - - *//
-        const data = await database.query(`UPDATE tb_regions SET region_name = $1, region_desc = $2, region_lat = $3, region_lng = $4, region_featured_city = $5 WHERE region_id = $6 RETURNING *`, [req.body.region_name, req.body.region_desc, req.body.region_lat, req.body.region_lng, req.body.region_featured_city, req.params.id]);
-        console.log(data);
+        const data = await database.query(`UPDATE tb_regions SET region_name = $1, region_desc = $2, region_lat = $3, region_lng = $4 WHERE region_id = $5 RETURNING *`, [req.body.region_name, req.body.region_desc, req.body.region_lat, req.body.region_lng, req.params.id]);
 
         //* - - - </> [DATA] </> - - - *//
         res.status(200).json({ status: "Region successfully updated", data: { region: data.rows[0]} });
